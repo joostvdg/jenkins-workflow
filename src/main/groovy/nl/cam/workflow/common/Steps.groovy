1650b8c12f5b5cb603900a863223afc126f4e855
Joost van der Griendt <joostvdg@gmail.com>
2015-11-25 16:34:56 +0100

    Maybe in this way?

--- src/main/groovy/nl/cam/workflow/common/Steps.groovy
+++ src/main/groovy/nl/cam/workflow/common/Steps.groovy
@@ -4,13 +4,12 @@ package nl.cam.workflow.common
  * Created by joost on 25-11-15.
  */
 
-class Steps {
 
-    static void npmBuild(){
-        stage 'Build'
-        node {
-            sh 'ls -lath'
-            sh 'npm install'
-        }
+def npmBuild() {
+    stage 'Build'
+    node {
+        sh 'ls -lath'
+        sh 'npm install'
     }
 }
+return this;
