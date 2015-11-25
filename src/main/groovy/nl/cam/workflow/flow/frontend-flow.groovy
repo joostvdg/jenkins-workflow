fdc323b1b07cf34edb32c9836bdf50afb9de3fc4
Joost van der Griendt <joostvdg@gmail.com>
2015-11-25 16:27:02 +0100

    Maybe in this way?

--- src/main/groovy/nl/cam/workflow/flow/frontend-flow.groovy
+++ src/main/groovy/nl/cam/workflow/flow/frontend-flow.groovy
@@ -4,14 +4,14 @@ package nl.cam.workflow.flow
  * Created by joost on 25-11-15.
  */
 
+import nl.cam.workflow.common.Steps
+
 stage 'Load common flow scripts'
 node {
     pwd()
     unstash 'flowFiles'
-    pwd()
-    sh 'ls -lath'
-    steps = load 'src/main/groovy/nl/cam/workflow/common/Steps.groovy'
 }
 
 // Execute the required steps
+Steps steps = new Steps()
 steps.npmBuild()
